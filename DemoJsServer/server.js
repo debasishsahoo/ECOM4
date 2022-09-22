6813a8ced2863053ba14f18b287af9fcff609d7d
debasishsahoo96 <debasish.sahoo96@gmail.com>
2022-09-22 23:19:02 +0530

    new Upadate on 22/09/22 1

---
+++ DemoJsServer/server.js
@@ -0,0 +1,56 @@
+const http = require('http');
+const server = http.createServer(
+    //Request:
+    (req, res) => {
+        const url = req.url
+        console.log('url:', url)
+
+        const method = req.method
+        console.log('method:', method)
+
+        if (url === '/') {
+            res.writeHead(200, { 'content-type': 'text/html' })
+            res.write(`<h1>Home Page</h1>`)
+            res.end()
+        }
+        else if (url === '/about') {
+            res.writeHead(200, { 'content-type': 'text/html' })
+            res.write(`<h1>About Page</h1>`)
+            res.end()
+        }
+        else if (url === '/info') {
+            res.writeHead(201, { 'content-type': 'text/plain' })
+            res.write('this is a Text Data')
+            res.end()
+        }
+        else if (url === '/data') {
+            res.writeHead(203, { 'content-type': 'text/xml' })
+            res.write(`<note>
+            <to>Tove</to>
+            <from>Jani</from>
+            <heading>Reminder</heading>
+            <body>Don't forget me this weekend!</body>
+            </note>`)
+            res.end()
+        }
+        else {
+            res.writeHead(404, { 'content-type': 'text/html' })
+            res.write(`<h1>Page not Foud</h1>`)
+            res.end()
+        }
+
+
+
+
+
+
+    }
+);
+server.listen(7000, () => { console.log(`Server Connected`) })
+
+
+//Domain => http://localhost
+
+//PORT => 7000
+
+// URL => /home or /user/123 or /prdouct/laptop/hp/omen/intel/i5
--- backend/project/index.ts
+++ backend/project/index.ts
@@ -3,28 +3,33 @@ import expess, { Request, Response } from 'express';
 const PORT: number = 7000;
 const HOST: string = 'localhost';
 
-const app = expess();
+//create Server with express function
+const server = expess();
 
-app.get('/', (req: Request, res: Response) => {
+server.get('/', (req: Request, res: Response) => {
   res.send('This is Home Page');
 });
 
-app.get('/about', (req: Request, res: Response) => {
+server.get('/about', (req: Request, res: Response) => {
   res.send('This is about Page');
 });
 
-app.get('/gallery', (req: Request, res: Response) => {
+server.get('/gallery', (req: Request, res: Response) => {
   res.send('This is gallery Page');
 });
 
-app.get('/contact', (req: Request, res: Response) => {
+server.get('/contact', (req: Request, res: Response) => {
   res.send('This is contact Page');
 });
 
-app.get('/info', (req: Request, res: Response) => {
+server.get('/info', (req: Request, res: Response) => {
   res.send('This is info Page');
 });
 
-app.listen(PORT, HOST, () => {
-  console.info(`Application Running on ${HOST}:${PORT} 😁👍`);
+server.get('*', (req: Request, res: Response) => {
+  res.send('Page Not Found');
+});
+
+server.listen(PORT, HOST, () => {
+  console.info(`server Running on ${HOST}:${PORT} 😁👍`);
 });
