04add87ee83e52ce0e5ddfb3b7aa0a202ac0147d
Debasish Sahoo <debasish.sahoo96@gmail.com>
2023-01-22 11:37:44 +0530

    new Update 22/01/23

---
+++ StudentMangementSystem/client/src-Backup-22-01-23/App.js
@@ -0,0 +1,56 @@
+import Demo1 from './Components/Demo1'
+import SingleProp from './Components/SingleProp';
+import MultipleProp from './Components/MultipleProp';
+import ValueProps from './Components/Value.prop'
+import PropsPass from './Components/PropsPass'
+import OptionalProps from './Components/OptionalProps'
+import PropsSpread from './Components/PropsSpread'
+import Parent from './Components/Parent'
+
+import Product from './ProductCard/Product';
+function App() {
+
+  const age = 30;
+  const income = 100000 * 20 % +(15000) + (2 * `${age}`)
+  const x = { a: 'AMIT', b: 'SUMAN', c: 'SAQ', d: 'Supa', e: 'rohi', f: 'retw', g: 'dev' }
+
+  return (
+    <>
+      {/* <Demo1 />
+      <SingleProp name='suman' />
+      <MultipleProp x="Amit" y="Suman" z="Rohit" />
+      <MultipleProp x="Amit" /> */}
+
+      {/* <ValueProps
+        strData="This is String Value"
+        templateData={`The age is ${age} and ${income}`}
+        numData={23}
+        boolData={false}
+        objData={{ name: 'Dev' }}
+        jsxData={45 % 5}
+        anyData={`${age}*${income} 👍`}
+      /> */}
+
+      {/* <PropsPass students={['Amit', 'Suman', 'Retwik', 'rohit', 'saq', 'Sup']} /> */}
+
+      {/* <OptionalProps name='Deb' />
+      <br />
+      <OptionalProps name='' /> */}
+
+      {/* <PropsSpread a={x.a} b={x.b} c={x.c} />
+      <hr />
+      <PropsSpread {...x} /> */}
+
+
+      {/* <Parent>
+        <Demo1 />
+      </Parent> */}
+
+      <Product />
+
+    </>
+
+  );
+}
+
+export default App;
