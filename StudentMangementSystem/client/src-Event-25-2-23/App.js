13e297fa772bffe4ddfb81a69813fce39b55cb43
Debasish Sahoo <debasish.sahoo96@gmail.com>
2023-02-25 15:30:59 +0530

    update 1

---
+++ StudentMangementSystem/client/src-Event-25-2-23/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import EventComponent from './components/EventComponent';
+
+
+function App() {
+  return (
+    <>
+      <EventComponent />
+    </>
+  );
+}
+
+export default App;
